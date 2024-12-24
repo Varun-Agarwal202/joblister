32c6663d1ddfce407d6e3b8739916f8b5bcb3a96
Varun-Agarwal202 <75946970+Varun-Agarwal202@users.noreply.github.com>
2024-12-23 23:36:16 -0700

    update roles

--- joblist/models.py
+++ joblist/models.py
@@ -11,7 +11,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     choices = ( ("Student", "Student"),
-               ("Employer", "Employer"))
+               ("Employer", "Employer"),
+               ("Mentor", "Mentor"))
     role = models.CharField(max_length=20, choices = choices)
 class JobListing(models.Model):
     jobtypechoices = ( ("Arts", "Arts"),
